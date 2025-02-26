980486a355579e175df24846b2d62f3dd05024e9
Anup Kumar Sahoo <designs.anup@gmail.com>
2025-02-26 14:36:39 +0000

    adding events to the responive navigation menu

--- 04_responsive_nav/app.js
+++ 04_responsive_nav/app.js
@@ -1,1 +1,10 @@
-console.log("responsive navigation");
+// console.log("responsive navigation");
+
+const togglebtn = document.querySelector('.menuIconWrap')
+const navMenu = document.querySelector('.nav')
+
+togglebtn.addEventListener('click', function(){
+    // console.log("nav button clicked");
+    togglebtn.classList.toggle('menuClose')
+    navMenu.classList.toggle('navStyle')
+})
