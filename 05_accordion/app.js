81ebf57b3fe1327b7cb04911597f8eb55680cf51
Anup Kumar Sahoo <designs.anup@gmail.com>
2025-02-27 20:40:22 +0000

    adding functionality to Accordion

--- 05_accordion/app.js
+++ 05_accordion/app.js
@@ -1,1 +1,18 @@
-console.log("welcome to code Accordion");
+// console.log("welcome to code Accordion");
+
+//using selectors inside the element
+const questions = document.querySelectorAll('.question')
+
+questions.forEach(function(que){
+    console.log(que);
+    // console.log(que.querySelector('.question-button'));
+    const btn = que.querySelector('.question-button')
+    
+    btn.addEventListener('click', function(){
+        que.classList.toggle('show-text')
+    })
+})
+
+
+
+// traversing the dom
