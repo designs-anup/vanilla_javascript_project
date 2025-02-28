4ce8a11f402d8e1c286c9d7f5f35525fee144180
Anup Kumar Sahoo <designs.anup@gmail.com>
2025-02-28 13:38:27 +0000

    only open the current section

--- 05_accordion/app.js
+++ 05_accordion/app.js
@@ -1,21 +1,29 @@
 // console.log("welcome to code Accordion");
-/*
+
 //using selectors inside the element
 const questions = document.querySelectorAll('.question')
 
 questions.forEach(function(que){
-    console.log(que);
+    // console.log(que);
     // console.log(que.querySelector('.question-button'));
     const btn = que.querySelector('.question-button')
     
     btn.addEventListener('click', function(){
+        // console.log(que);
+        questions.forEach(function(item){
+            // console.log(item);
+            if(item != que){
+                item.classList.remove('show-text')
+            }
+        })
+
         que.classList.toggle('show-text')
     })
 })
-*/
+
 
 // traversing the dom
-
+/*
 const btns = document.querySelectorAll('.question-button')
 // console.log(btns);
 
@@ -26,3 +34,4 @@ btns.forEach(function(btn){
         e.currentTarget.parentElement.parentElement.classList.toggle('show-text')
     })
 })
+*/    
