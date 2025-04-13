4760119634dfe116e0fcac2716d3abfe7fc0d929
Anup Kumar Sahoo <designs.anup@gmail.com>
2025-04-13 04:39:12 +0000

    adding buttons to start and stop intervals to chnage background color

--- 01_color_flipper/app.js
+++ 01_color_flipper/app.js
@@ -1,9 +1,11 @@
 const animal = ["dog","elephant","fox","horse","owl"]
-const hexColor = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+// const hexColor = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
 
 const btn = document.getElementById('btn')
 const btnHex = document.getElementById('btnHex')
 const color = document.querySelector('.colorPan')
+const btnStart = document.querySelector('#start')
+const btnStop = document.querySelector('#stop')
 
 flipBackgroundImg = function(){
     let randomBg = pickRandomBg(animal)
@@ -13,6 +15,8 @@ flipBackgroundImg = function(){
 }
 
 flipBackgroundColor = function(){
+    // generate random color
+    const hexColor = "0123456789ABCDEF"
     let hex = "#"
     
     for(let i = 0; i < 6; i++){
@@ -23,13 +27,28 @@ flipBackgroundColor = function(){
     // console.log("hex : ", hex);
     color.textContent = hex
     document.body.style.background = hex
+
+    return hex
 }
 
 function pickRandomBg(item){
     return Math.floor(Math.random() * item.length)
 }
 
+let intervalId
 
+startChangeBackground = function(){
+    intervalId = setInterval(changeBGColor, 1000)
+    function changeBGColor(){
+        document.body.style.background = flipBackgroundColor()
+    }
+}
+
+stopChangeBackground = function(){
+    clearInterval(intervalId)
+}
 
 btn.addEventListener('click', flipBackgroundImg)
 btnHex.addEventListener('click', flipBackgroundColor)
+btnStart.addEventListener('click', startChangeBackground)
+btnStop.addEventListener('click', stopChangeBackground)
